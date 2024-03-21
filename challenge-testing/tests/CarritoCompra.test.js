af9420f4882da7b80b6dcaa7a420158ac71a075a
LNicolasOcana <locana392@gmail.com>
2024-03-21 09:59:11 -0300

    homework-final

--- challenge-testing/tests/CarritoCompra.test.js
+++ challenge-testing/tests/CarritoCompra.test.js
@@ -8,24 +8,24 @@ describe("CarritoCompra", () => {
   });
 
   test("Agregar producto al carrito", () => {
-    carrito.agregarProducto({ nombre: "Camisa", precio: 20 });
+    carrito.agregarProducto({ nombre: "REMERA", precio: 20 });
     expect(carrito.productos.length).toBe(1);
   });
 
   test("Calcular total de la compra", () => {
-    carrito.agregarProducto({ nombre: "Camisa", precio: 20 });
-    carrito.agregarProducto({ nombre: "Pantalón", precio: 30 });
+    carrito.agregarProducto({ nombre: "REMERA", precio: 20 });
+    carrito.agregarProducto({ nombre: "BERMUDA", precio: 30 });
     expect(carrito.calcularTotal()).toBe(50);
   });
 
   test("Aplicar descuento al total de la compra", () => {
-    carrito.agregarProducto({ nombre: "Camisa", precio: 20 });
-    carrito.agregarProducto({ nombre: "Pantalón", precio: 30 });
+    carrito.agregarProducto({ nombre: "REMERA", precio: 20 });
+    carrito.agregarProducto({ nombre: "BERMUDA", precio: 30 });
     expect(carrito.aplicarDescuento(10)).toBe(45);
   });
 
   test("Aplicar descuento del 100%", () => {
-    carrito.agregarProducto({ nombre: "Camisa", precio: 20 });
+    carrito.agregarProducto({ nombre: "REMERA", precio: 20 });
     expect(carrito.aplicarDescuento(100)).toBe(0);
   });
 
